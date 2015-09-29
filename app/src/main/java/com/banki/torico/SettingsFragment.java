1fb366350a33dde83ea61c19fffd07c09ba44825
Banki <banki@altoqi.com.br>
2015-09-29 13:25:00 -0300

    Incluída ActionBar na tela Settings

---
+++ app/src/main/java/com/banki/torico/SettingsFragment.java
@@ -0,0 +1,53 @@
+package com.banki.torico;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsFragment extends PreferenceFragment  implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private final String[] mAutoSummaryFields = { "salario_bruto", "horas_mensais", "adicional_extra" };
+    private final int mEntryCount = mAutoSummaryFields.length;
+    private CustomEditPreference[] mPreferenceEntries;
+
+    @Override
+    public void onCreate(final Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+
+        mPreferenceEntries = new CustomEditPreference[mEntryCount];
+        for (int i = 0; i < mEntryCount; i++) {
+            mPreferenceEntries[i] = (CustomEditPreference)getPreferenceScreen().findPreference(mAutoSummaryFields[i]);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        for (int i = 0; i < mEntryCount; i++) {
+            updateSummary(mAutoSummaryFields[i]);
+        }
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        updateSummary(key);
+    }
+
+    private void updateSummary(String key) {
+        for (int i = 0; i < mEntryCount; i++) {
+            if (key.equals(mAutoSummaryFields[i])) {
+                mPreferenceEntries[i].updateSummary();
+                break;
+            }
+        }
+    }
+}
